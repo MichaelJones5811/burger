05b264c7b8e4bd0036a10469e50a6bdfa1d5484e
Michael Jones <mjones5811@aol.com>
2017-05-30 16:41:53 -0400

    push before class

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var dotenv = require("dotenv").config();
+
+var mysql = require("mysql");
+var env = require("../.env");
+//var password_DB = process.env.DB_PASS;
+var connection = mysql.createConnection({
+
+  host: "localhost",
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS, 
+  // user: "root", 
+  // password: "Jordan23!", 
+  database: "burgers_db"
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+
+ 
+module.exports = connection;
