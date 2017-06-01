56160a59b1d9e59b431aa0106102d88d50fc7adc
Michael Jones <mjones5811@aol.com>
2017-06-01 09:45:54 -0400

    push final (final for now) version

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,6 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: process.env.DB_USER, 
   password: process.env.DB_PASS, 
-  // user: "root", 
-  // password: "Jordan23!", 
   database: "burgers_db"
 });
  
